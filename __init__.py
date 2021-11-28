e2706692daa1378408b3d49edfece609cdce1a8b
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-11-28 18:22:17 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,19 +16,19 @@ class princessSkill(MycroftSkill):
 
     @intent_handler('princess.intent')
     def handle_not_are_you_intent(self, message):
+        #serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        #serA.flush()
+        #serA.write(b"wave")
+        #serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+        #serB.flush()
+        #serB.write(b"wave")        
+        #serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        #serC.flush()
+        #serC.write(b"wave")
+        #serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        #serD.flush()
+        #serD.write(b"wave")
         self.speak_dialog("No I am just a knight but look at my wave.")
-        serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        serA.flush()
-        serA.write(b"princess")
-        serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
-        serB.flush()
-        serB.write(b"princess")        
-        serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
-        serC.flush()
-        serC.write(b"princess")
-        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
-        serD.flush()
-        serD.write(b"princess")
 
     def stop(self):
         pass
